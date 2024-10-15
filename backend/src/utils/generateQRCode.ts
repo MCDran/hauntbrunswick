b1dda75bb5a754209271b1b3f0894831b73effb7
Tucker Davis <17817381+tdavis298@users.noreply.github.com>
2024-10-14 23:30:09 -0400

    feat: Add QR code generation and emailing functionality
    Integrated QR code generation for registrations and sending confirmation emails with QR code attachments. Added new utility functions and enhanced the registration and cancellation processes in the backend. Updated frontend assets and configurations. Finished theming and responsive design updating for mobile.

--- backend/src/utils/generateQRCode.ts
+++ backend/src/utils/generateQRCode.ts
@@ -4,8 +4,8 @@ import QRCode from 'qrcode';
 export async function generateQRCode(registrationNumber: string, email: string): Promise<string> {
     try {
         const qrCodeData = `${registrationNumber}:${email}`; // Data to encode
-        const qrCodeURL = await QRCode.toDataURL(qrCodeData); // Generates base64 image URL
-        return qrCodeURL; // Return the generated QR code URL
+         // Generates base64 image URL
+        return QRCode.toDataURL(qrCodeData); // Return the generated QR code URL
     } catch (err) {
         console.error('Failed to generate QR Code', err);
         throw err; // Ensure that an error is thrown if generation fails
